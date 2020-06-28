2e8f5b9349379d2b159e0a96698c3feecdb40787
zubeir <zubkayare@gmail.com>
2020-06-28 23:13:23 +0300

    adding the designing

---
+++ register-sw.js
@@ -0,0 +1,9 @@
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+      navigator.serviceWorker.register("/service-worker.js").then(
+        function (registration) {},
+        function (err) {}
+      );
+    });
+  }
+  
